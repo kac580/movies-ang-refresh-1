007252c1ec6e3337d8f0c8e5f8207a3b3230cf88
Kyra Buchmann <kac580@outlook.com>
2020-01-23 20:40:18 -0600

    added angular.core & .platform-browser & ability to add a movie

--- src/app/movies-list/movies-list.component.ts
+++ src/app/movies-list/movies-list.component.ts
@@ -10,16 +10,23 @@ export class MoviesListComponent implements OnInit {
 
   @Input() movies: Movie[];
   @Input() name: string;
+ 
+  newTitle: string;
+  newCategory: string;
+  newYear: number;
 
   constructor() { 
-
   }
 
   ngOnInit() {
   }
 
-  details(movie: Movie) {
+  details(movie: Movie){
     alert(`details for ${movie.title}`);
+  }
+  addMovie() {
+    let movie = new Movie (this.newTitle, this.newCategory, this.newYear);
+    this.movies.push(movie);
   }
 
 }
